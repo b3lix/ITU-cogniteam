31e02736578bb4105e95d84fdc88f63463ae2ae5
b3lix <erik.belko1@gmail.com>
2021-12-01 23:49:09 +0100

    Add simple ExpandableList filled with example data, add new layouts

--- frontend-android/app/src/main/java/com/app/itu/ui/home/HomeFragment.java
+++ frontend-android/app/src/main/java/com/app/itu/ui/home/HomeFragment.java
@@ -1,17 +1,25 @@
 package com.app.itu.ui.home;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.app.itu.JsonRequest;
+import com.app.itu.R;
 import com.app.itu.Singleton;
 import com.app.itu.databinding.FragmentHomeBinding;
 
@@ -22,6 +30,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.HashMap;
 
 public class HomeFragment extends Fragment{
 
@@ -41,35 +50,78 @@ public class HomeFragment extends Fragment{
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        ListView listView = (ListView) binding.listView;
-        List list = new ArrayList();
+        ExpandableListView expandableListView;
+        ExpandableListAdapter expandableListAdapter;
+        List<String> expandableListTitle;
+        HashMap<String, List<String>> expandableListDetail;
 
-        Singleton.getInstance().setUrlOperation("/food/get");
-        jsonRequest.getMethod(root.getContext(), new JsonRequest.VolleyCallBack() {
+        expandableListView = (ExpandableListView) binding.expandableListView;
+        expandableListDetail = ExpandableListDataPump.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListAdapter = new CustomExpandableListAdapter(getContext(), expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
             @Override
-            public void onSuccess() throws JSONException {
-
-                JSONObject jsonObject = new JSONObject(Singleton.getInstance().jsonOut);
-                JSONArray tmp = jsonObject.getJSONArray("food");
-
-                for (int i =0; i < tmp.length(); i++)
-                {
-                    JSONObject object = tmp.getJSONObject(i);
-                    list.add(object.get("name"));
-
-                }
-                ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, list);
-                listView.setAdapter(adapter);
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
-        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
 
             }
         });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+
+//        Singleton.getInstance().setUrlOperation("/food/get");
+//        jsonRequest.getMethod(root.getContext(), new JsonRequest.VolleyCallBack() {
+//            @Override
+//            public void onSuccess() throws JSONException {
+//
+//                JSONObject jsonObject = new JSONObject(Singleton.getInstance().jsonOut);
+//                JSONArray tmp = jsonObject.getJSONArray("food");
+//
+//                for (int i =0; i < tmp.length(); i++)
+//                {
+//                    JSONObject object = tmp.getJSONObject(i);
+//                    list.add(object.get("name"));
+//
+//                }
+//                ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_expandable_list_item_1, list);
+//                listView.setAdapter(adapter);
+//            }
+//        });
+//
+//        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//
+//            }
+//        });
         return root;
     }
 
@@ -82,5 +134,160 @@ public class HomeFragment extends Fragment{
     static public void appendItems(List list, ArrayAdapter adapter)
     {
 
+    }
+}
+
+class ExpandableListDataPump {
+    public static HashMap<String, List<String>> getData() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+
+        List<String> cricket = new ArrayList<String>();
+        cricket.add("India");
+        cricket.add("Pakistan");
+        cricket.add("Australia");
+        cricket.add("England");
+        cricket.add("South Africa");
+
+        List<String> football = new ArrayList<String>();
+        football.add("Brazil");
+        football.add("Spain");
+        football.add("Germany");
+        football.add("Netherlands");
+        football.add("Italy");
+
+        List<String> basketball = new ArrayList<String>();
+        basketball.add("United States");
+        basketball.add("Spain");
+        basketball.add("Argentina");
+        basketball.add("France");
+        basketball.add("Russia");
+
+        List<String> a = new ArrayList<String>();
+        a.add("India");
+
+        List<String> b = new ArrayList<String>();
+        b.add("India");
+
+        List<String> c = new ArrayList<String>();
+        c.add("India");
+
+        List<String> d = new ArrayList<String>();
+        d.add("India");
+
+        List<String> e = new ArrayList<String>();
+        e.add("India");
+
+        List<String> f = new ArrayList<String>();
+        f.add("India");
+
+        List<String> g = new ArrayList<String>();
+        g.add("India");
+
+        List<String> h = new ArrayList<String>();
+        h.add("India");
+
+        List<String> i = new ArrayList<String>();
+        i.add("India");
+
+        expandableListDetail.put("CRICKET TEAMS", cricket);
+        expandableListDetail.put("FOOTBALL TEAMS", football);
+        expandableListDetail.put("BASKETBALL TEAMS", basketball);
+        expandableListDetail.put("a", a);
+        expandableListDetail.put("b", b);
+        expandableListDetail.put("c", c);
+        expandableListDetail.put("d", d);
+        expandableListDetail.put("e", e);
+        expandableListDetail.put("f", f);
+        expandableListDetail.put("g", g);
+        expandableListDetail.put("h", h);
+        expandableListDetail.put("i", i);
+        return expandableListDetail;
+    }
+}
+
+class CustomExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> expandableListTitle;
+    private HashMap<String, List<String>> expandableListDetail;
+
+    public CustomExpandableListAdapter(Context context, List<String> expandableListTitle,
+                                       HashMap<String, List<String>> expandableListDetail) {
+        this.context = context;
+        this.expandableListTitle = expandableListTitle;
+        this.expandableListDetail = expandableListDetail;
+    }
+
+    @Override
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .get(expandedListPosition);
+    }
+
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
+
+    @Override
+    public View getChildView(int listPosition, final int expandedListPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+        TextView expandedListTextView = (TextView) convertView
+                .findViewById(R.id.expandedListItem);
+        expandedListTextView.setText(expandedListText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.expandableListTitle.get(listPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.expandableListTitle.size();
+    }
+
+    @Override
+    public long getGroupId(int listPosition) {
+        return listPosition;
+    }
+
+    @Override
+    public View getGroupView(int listPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_group, null);
+        }
+        TextView listTitleTextView = (TextView) convertView
+                .findViewById(R.id.listTitle);
+        listTitleTextView.setTypeface(null, Typeface.BOLD);
+        listTitleTextView.setText(listTitle);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
     }
 }
