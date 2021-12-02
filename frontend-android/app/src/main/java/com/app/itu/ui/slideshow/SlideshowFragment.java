6961014be398840635845a6a6cbb458e442332a4
b3lix <erik.belko1@gmail.com>
2021-12-02 23:43:15 +0100

    Add Favourites, used SlideshowFragment for that

--- frontend-android/app/src/main/java/com/app/itu/ui/slideshow/SlideshowFragment.java
+++ frontend-android/app/src/main/java/com/app/itu/ui/slideshow/SlideshowFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,27 +15,90 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.app.itu.JsonRequest;
 import com.app.itu.R;
+import com.app.itu.Singleton;
 import com.app.itu.databinding.FragmentSlideshowBinding;
+import com.app.itu.ui.home.CustomExpandableListAdapter;
+import com.app.itu.ui.home.ExpandableListDataPump;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
     private FragmentSlideshowBinding binding;
+    JsonRequest jsonRequest = new JsonRequest();
+    private com.app.itu.ui.home.ExpandableListDataPump expandableListDataPump = new ExpandableListDataPump();
+
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        slideshowViewModel = new ViewModelProvider(this).get(SlideshowViewModel.class);
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        expandableListView = (ExpandableListView) binding.expandableListView;
+
+        Singleton.getInstance().setUrlOperation("/food/get");
+        jsonRequest.getMethod(root.getContext(), new JsonRequest.VolleyCallBack() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onSuccess() throws JSONException {
+
+                JSONObject jsonObject = new JSONObject(Singleton.getInstance().jsonOut);
+                JSONArray tmp = jsonObject.getJSONArray("food");
+
+                expandableListDetail = expandableListDataPump.getData(tmp);
+                expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+                expandableListAdapter = new CustomExpandableListAdapter(root.getContext(), expandableListTitle, expandableListDetail);
+                expandableListView.setAdapter(expandableListAdapter);
+            }
+        }, true);
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(root.getContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(root.getContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        root.getContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
             }
         });
         return root;
